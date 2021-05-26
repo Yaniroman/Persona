b25fdc0d8d52dae038ebc4c99baeb1cada913250
Yaniroman <yanina.roman@reserv.com.ar>
2021-05-26 11:49:20 -0300

    cambios

--- src/Principal.java
+++ src/Principal.java
@@ -2,8 +2,8 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		Persona persona1 = new Persona("Yani", 34);
-		Persona persona2 = new Persona("Guille", 35);
+		Persona persona1 = new Persona("Yani", "Roman",32448956,34,168,80);
+		Persona persona2 = new Persona("Guille", "Fink", 32022487,35,190,110);
 		System.out.println(persona1.nombre + " " + persona1.edad);
 		System.out.println(persona2.nombre);
 		System.out.println(persona1.edad);
